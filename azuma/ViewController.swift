357777cddcff58cb787fa97b2f5a9c4755227178
azaz-el <sear.azazel@gmail.com>
2019-01-25 00:09:35 +0900

    feat: タップイベント追加

--- azuma/ViewController.swift
+++ azuma/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var count: NSInteger = 0
+    
+    @IBAction func onButtonTap(_ sender: Any) {
+        count += 1
+        label.text = count.description + "回タップされました"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
