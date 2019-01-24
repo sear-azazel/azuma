c2b48188e6a3f885ce4ab2bf6e7b46c49c9b5179
azaz-el <sear.azazel@gmail.com>
2019-01-24 23:06:50 +0900

    Initial Commit

---
+++ azuma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  azuma
+//
+//  Created by 東隆一 on 2019/01/24.
+//  Copyright © 2019 kotaroinc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
